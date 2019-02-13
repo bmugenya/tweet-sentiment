a9b296eef648c2133d6866254de0531a285a6e78
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-02-13 20:52:59 +0300

    [Feature #163953443] specified user to get tweets from

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -7,6 +7,27 @@ from tweepy import Stream
 import twitter_credentials
 
 
+class TwitterClient():
+
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
 class TwitterAuthenticator():
 
     def authenticate_twitter_app(self):
@@ -62,5 +83,8 @@ if __name__ == "__main__":
     hash_tag = ["100DaysOfCode"]
     fetched_tweets_filename = "tweets.json"
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag)
+    twitter_client = TwitterClient('100xcode')
+    print(twitter_client.get_user_timeline_tweets(1))
+
+    # twitter_streamer = TwitterStreamer()
+    # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag)
