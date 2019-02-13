101c78a1bf1e60caa261469e7c4e8a24ed86a99c
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-02-13 20:02:22 +0300

    created a class for authentication

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,3 +1,5 @@
+from tweepy import API
+from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -5,22 +7,33 @@ from tweepy import Stream
 import twitter_credentials
 
 
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+
+
 class TwitterStreamer():
     """
     A class for streaming and processing live tweets
     """
 
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+
     def stream_tweets(self, fetched_tweets_filename, hash_tag):
         # This handles Twitter authentication and the connection to the Twitter Streamin API.
-        listener = StdOutListerner(fetched_tweets_filename)
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        listener = TwitterListerner(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
+
         stream = Stream(auth, listener)
 
         stream.filter(track=hash_tag)
 
 
-class StdOutListerner(StreamListener):
+class TwitterListerner(StreamListener):
     """
     This is a basic listener class that just prints recieved tweets to stdout
     """
@@ -31,7 +44,7 @@ class StdOutListerner(StreamListener):
     def on_data(self, data):
         try:
             print(data)
-            with open(self.fetched_tweets_filename, 'w') as tf:
+            with open(self.fetched_tweets_filename, 'a') as tf:
                 tf.write(data)
             return True
         except BaseException as e:
@@ -39,6 +52,9 @@ class StdOutListerner(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            # returning fasle on_data method in case rate limit occurs
+            return False
         print(status)
 
 
